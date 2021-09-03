915e0e70f760fc9ed75ea0344020f377dfe5b75c
motonori19950629 <s2.k5.i2.a2.a@ezweb.ne.jp>
2021-09-03 19:07:16 +0900

    guard使用

--- Ex5/ViewController.swift
+++ Ex5/ViewController.swift
@@ -18,27 +18,27 @@ class ViewController: UIViewController {
 
     // 計算実行ボタン
     @IBAction func calcButton(_ sender: Any) {
-        // 各String?をDouble?に変換しDoubleに変換。未入力や文字列が入力された際は0に変換
-        let value1 = Double(number1TextField.text ?? "") ?? 0
-        let value2 = Double(number2TextField.text ?? "") ?? 0
-
         // 入力不備でmessageをshowAlertに送る
-        if number1TextField.text == "" { // 被除数空白
+        guard let value1 = Double(number1TextField.text!) else { // 被除数未入力
             showAlert(message: "被除数を入力してください")
-        } else if number2TextField.text == "" { // 序数空白
-            showAlert(message: "除数を入力してください")
-        } else if value2 == 0 { // 序数0
-            showAlert(message: "除数は0以外の数字を入力してください")
-        } else { // 計算と表示の実行
-            let result = String(value1 / value2)
-            resultLabel.text = result
+            return
         }
+        guard let value2 = Double(number2TextField.text!) else { // 除数未入力
+            showAlert(message: "除数を入力してください")
+            return
+        }
+        guard value2 != 0 else { // 除数0
+            showAlert(message: "除数は0以外の数字を入力してください")
+            return
+        }
+
+        let result = String(value1 / value2)
+        resultLabel.text = result
     }
 
+    // エラーメッセージを受け取ってアラート表示
     func showAlert(message: String) {
-        // アラートの設定。message以外は同じ。
-        let title = "課題５"
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: "課題５", message: message, preferredStyle: .alert)
         let okAlert = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAlert)
         present(alert, animated: true, completion: nil)
