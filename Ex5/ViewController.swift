eff21d4c0500b3b1f31542e073a2771e7bea96a4
akio0911 <akio0911@gmail.com>
2021-09-06 16:44:04 +0900

    feedback

--- Ex5/ViewController.swift
+++ Ex5/ViewController.swift
@@ -14,21 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet private weak var number2TextField: UITextField!
 
     // 結果ラベル
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
 
     // 計算実行ボタン
     @IBAction func calcButton(_ sender: Any) {
         // 入力不備でmessageをshowAlertに送る
         guard let value1 = Double(number1TextField.text!) else { // 被除数未入力
-            showAlert(message: "被除数を入力してください")
+            showAlert(message: "割られる数を入力して下さい")
             return
         }
         guard let value2 = Double(number2TextField.text!) else { // 除数未入力
-            showAlert(message: "除数を入力してください")
+            showAlert(message: "割る数を入力して下さい")
             return
         }
         guard value2 != 0 else { // 除数0
-            showAlert(message: "除数は0以外の数字を入力してください")
+            showAlert(message: "割る数には0を入力しないで下さい")
             return
         }
 
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
     }
 
     // エラーメッセージを受け取ってアラート表示
-    func showAlert(message: String) {
+    private func showAlert(message: String) {
         let alert = UIAlertController(title: "課題５", message: message, preferredStyle: .alert)
         let okAlert = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAlert)
